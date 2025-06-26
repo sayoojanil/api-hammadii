2f96c47a347647c1d41d006f425b3ccae44b2944
sayooj anil <sayoojbgmi@gmail.com>
2025-06-26 23:16:40 +0530

    db changed to mongodb

--- src/datasources/db.datasource.ts
+++ src/datasources/db.datasource.ts
@@ -2,16 +2,19 @@ import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
 import {juggler} from '@loopback/repository';
 
 const config = {
-  name: 'db',
+  name: 'mongodb',
   connector: 'mongodb',
   url: 'mongodb+srv://Payingguest:alankarvp100@cluster0.tfrej4l.mongodb.net/Payingguest?retryWrites=true&w=majority&appName=Cluster0',
-  useNewUrlParser: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 };
+
+
 
 // Observe application's life cycle to disconnect the datasource when
 // application is stopped. This allows the application to be shut down
 // gracefully. The `stop()` method is inherited from `juggler.DataSource`.
-// Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html 
+// Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
 @lifeCycleObserver('datasource')
 export class DbDataSource extends juggler.DataSource
   implements LifeCycleObserver {
