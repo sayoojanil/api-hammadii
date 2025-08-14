03f3d7a3be36df826e69f9adebe55bcb96e51264
sayooj anil <sayoojbgmi@gmail.com>
2025-08-14 23:42:56 +0530

    done

--- src/controllers/payment.controller.ts
+++ src/controllers/payment.controller.ts
@@ -32,7 +32,7 @@ export class PaymentController {
             properties: {
               name: { type: 'string' },
               amount: { type: 'number' },
-              status: { type: 'string', enum: ['paid', 'overdue'] },
+              status: { type: 'string', enum: ['paid', 'overdue','Awaiting_payment'] },
               date: { type: 'string', format: 'date' },
               duedate: { type: 'string', format: 'date' },
               paymentMethod: { type: 'string' },
@@ -67,7 +67,7 @@ export class PaymentController {
     if (isNaN(payment.amount) || payment.amount < 0) {
       throw new HttpErrors.BadRequest('Invalid amount');
     }
-    if (!['paid', 'overdue'].includes(payment.status)) {
+    if (!['paid', 'overdue','Awaiting_payment'].includes(payment.status)) {
       throw new HttpErrors.BadRequest('Invalid status');
     }
     if (isNaN(Date.parse(payment.date)) || isNaN(Date.parse(payment.duedate))) {
@@ -88,7 +88,7 @@ export class PaymentController {
             properties: {
               name: { type: 'string' },
               amount: { type: 'number' },
-              status: { type: 'string', enum: ['paid', 'overdue'] },
+              status: { type: 'string', enum: ['paid', 'overdue','Awaiting_payment'] },
               date: { type: 'string', format: 'date' },
               duedate: { type: 'string', format: 'date' },
               paymentMethod: { type: 'string' },
@@ -114,7 +114,7 @@ export class PaymentController {
     if (payment.amount && (isNaN(payment.amount) || payment.amount < 0)) {
       throw new HttpErrors.BadRequest('Invalid amount');
     }
-    if (payment.status && !['paid', 'overdue'].includes(payment.status)) {
+    if (payment.status && !['paid', 'overdue','Awaiting_payment'].includes(payment.status)) {
       throw new HttpErrors.BadRequest('Invalid status');
     }
     if (payment.date && isNaN(Date.parse(payment.date))) {
